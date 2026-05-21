6c5e469bebc0a7a08f92a4739e68940887d56664
Hanyang Li <62730304+hylsql@users.noreply.github.com>
2026-05-21 13:06:15 -0600

    Update Create a clean_inventory view.sql

--- data_cleaning/Create a clean_inventory view.sql
+++ data_cleaning/Create a clean_inventory view.sql
@@ -3,9 +3,8 @@
 CREATE OR REPLACE VIEW clean_inventory AS 
 	SELECT	inventory_id,
 			product_id,
-			CASE	WHEN LOWER(TRIM(warehouse)) IN ('south jordan','SOUTH JORDAN') THEN 'South Jordan'
-					WHEN LOWER(TRIM(warehouse)) IN ('Draper','DRPR') THEN 'Draper'
-					WHEN LOWER(TRIM(warehouse)) IN ('Salt Lake City','SLC') THEN 'Salt Lake City'
+			CASE	WHEN LOWER(TRIM(warehouse)) IN ('draper','drpr') THEN 'Draper'
+					WHEN LOWER(TRIM(warehouse)) IN ('salt lake city','slc') THEN 'Salt Lake City'
 					ELSE INITCAP(TRIM(warehouse)) END AS warehouse,
 			CASE	WHEN quantity_on_hand < 0 THEN 0
 					ELSE quantity_on_hand END AS quantity_on_hand,
